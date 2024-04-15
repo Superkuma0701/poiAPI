faf74debf4dcf3d9de72b83819c8fd2979182867
FelixWei0701 <felix.wei@tpisoftware.com>
2024-04-15 16:42:00 +0800

    init poi test

---
+++ src/test/java/com/git/poitest/PoiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.poitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
